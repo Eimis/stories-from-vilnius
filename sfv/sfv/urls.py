a06f7e176471a5bfb82e0f7046d82a06b610d836
Eimantas <eimantas.stonys@gmail.com>
2015-04-14 21:15:42 +0300

    Fix PEP 8 errors

--- sfv/sfv/urls.py
+++ sfv/sfv/urls.py
@@ -1,28 +1,34 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from sfv.views import landing, Login, logout, redirect_to_login, redirect_to_stories, Stories, User_stories, User_story, New_story
+from sfv.views import landing
+from sfv.views import Login
+from sfv.views import logout
+from sfv.views import redirect_to_login
+from sfv.views import redirect_to_stories
+from sfv.views import Stories
+from sfv.views import User_stories
+from sfv.views import User_story
+from sfv.views import New_story
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-
-	#App urls:
+urlpatterns = patterns(
+    '',
     url(r'^landing/?$', landing),
     url(r'^$', Login),
     url(r'^accounts/login/?$', redirect_to_login),
-    url(r'^stories/?$', Stories),
+    url(r'^stories/?$', Stories, name='stories'),
     url(r'^logout/?$', logout),
     url(r'^story/?$', New_story),
     url(r'^story/(?P<slug>[-\w]+)/?$', redirect_to_stories),
     url(r'^(?P<username>[-\w]+)/?$', User_stories),
     url(r'^(?P<username>[-\w]+)/story/(?P<slug>[-\w]+)/?$', User_story),
 
-    #Facebook urls:
+    # Facebook urls:
     (r'^facebook/', include('django_facebook.urls')),
 
-
-    #admin
+    # admin
     url(r'^site/admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
